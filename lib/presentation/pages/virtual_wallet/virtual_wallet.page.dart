1418f998138f98088a62c34aa88bcbb8af9c6c91
AndresChavezR <acchavezr@rtisas.com.co>
2023-04-15 21:02:49 -0500

    Ventana de info recargar

--- lib/presentation/pages/virtual_wallet/virtual_wallet.page.dart
+++ lib/presentation/pages/virtual_wallet/virtual_wallet.page.dart
@@ -25,114 +25,143 @@ class VirtualWalletPage extends StatelessWidget {
         child: SingleChildScrollView(
             physics: const AlwaysScrollableScrollPhysics(),
             child: Obx(() {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),child: const _CustomVirtualWalletWidget()),
-                    Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                        child: const Text(
-                          'Historial de abonos',
-                          style:
-                              TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                        )),
-                    ...virtualCtrl.historyRecharge.value
-                        .map((recharge) => Container(
-                          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                              decoration: BoxDecoration(
-                                border: Border.all(color: Color.fromARGB(255, 19, 73, 20))
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                      child: const _CustomVirtualWalletWidget()),
+                  Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 15),
+                      child: const Text(
+                        'Historial de abonos',
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold),
+                      )),
+                  ...virtualCtrl.historyRecharge.value
+                      .map((recharge) => Container(
+                            margin: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 10),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Color.fromARGB(255, 19, 73, 20))),
+                            child: ListTile(
+                              leading: Container(
+                                  height: double.infinity,
+                                  child: Icon(
+                                    Icons.monetization_on,
+                                    color: Colors.green,
+                                  )),
+                              title: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(recharge.paymentDescription),
+                                  Text(recharge.paymentDate),
+                                ],
                               ),
-                              child: ListTile(
-                                leading: Container(
-                                    height: double.infinity,
-                                    child: Icon(Icons.monetization_on, color: Colors.green,)),
-                                title: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(recharge.paymentDescription),
-                                    Text(recharge.paymentDate),
-                                  ],
-                                ),
-                                subtitle: Text(Helpers.formatCurrency(
-                                    recharge.paymentValue.toDouble())),
-                              ),
-                            ))
-                        .toList(),
-                  ],
-                );
-              }
-            )),
+                              subtitle: Text(Helpers.formatCurrency(
+                                  recharge.paymentValue.toDouble())),
+                            ),
+                          ))
+                      .toList(),
+                ],
+              );
+            })),
       ),
     );
   }
 }
 
 class _CustomVirtualWalletWidget extends StatelessWidget {
-  const _CustomVirtualWalletWidget({
-    super.key,
-  });
+  const _CustomVirtualWalletWidget();
 
   @override
   Widget build(BuildContext context) {
     final virtualCtrl = Get.find<VirtualWalletController>();
 
-    return Container(
-      child: (virtualCtrl.userContainVirtualWallet.value)
-          ? SizedBox(
-              // width: SizeConfig.safeBlockSizeHorizontal(90),
-              // height: SizeConfig.safeBlockSizeHorizontal(40),
-              child: Card(
-                // margin: const EdgeInsets.all(20),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            'Saldo',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w500),
-                          ),
-                          OutlinedButton(
-                              onPressed: () {}, child: Text('Recargar'))
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Obx(() {
-                            return Text(
-                                Helpers.formatCurrency(virtualCtrl.virtualWalletPassenger.value?.currentValue.toDouble() ?? 0.0),
-                                style: const TextStyle(
-                                    fontSize: 30, fontWeight: FontWeight.w400));
-                          }),
-                        ],
-                      ),
-                      Obx(() {
-                        return RichText(
-                          text: TextSpan(
-                            text: 'Tú última recarga fue de: ',
-                            style: DefaultTextStyle.of(context).style,
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text:
-                                      '\$${virtualCtrl.virtualWalletPassenger.value?.lastRechargeValue}',
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.bold)),
-                            ],
-                          ),
-                        );
-                      })
-                    ],
+    return Obx(() {
+      return Container(
+        child: (virtualCtrl.userContainVirtualWallet.value)
+            ? SizedBox(
+                child: Card(
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'Saldo',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w500),
+                            ),
+                            OutlinedButton(
+                                onPressed: () {
+                                  showDialog(
+                                      context: context,
+                                      builder: (context) {
+                                        return  AlertDialog(
+                                          title: const Text('Información'),
+                                          content: Column(
+                                            mainAxisSize: MainAxisSize.min,
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: const [
+                                              Text('Para nosotros es importante tu seguridad, es por ello necesario comunicarse a través de los siguientes canales de información para solicitar una recarga de tu billetera virtual.'),
+                                              Divider(),
+                                              Text('info@rtisas.com.co', style: TextStyle(fontWeight: FontWeight.w600, fontSize: 13),),
+                                              Text('recursosbiiletera@rtisas.com', style: TextStyle(fontWeight: FontWeight.w600, fontSize: 13)),
+                                            ],
+                                          ),
+                                        );
+                                      });
+                                },
+                                child: const Text('Recargar'))
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Obx(() {
+                              return Text(
+                                  Helpers.formatCurrency(virtualCtrl
+                                          .virtualWalletPassenger
+                                          .value
+                                          ?.currentValue
+                                          .toDouble() ??
+                                      0.0),
+                                  style: const TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.w400));
+                            }),
+                          ],
+                        ),
+                        Obx(() {
+                          return RichText(
+                            text: TextSpan(
+                              text: 'Tú última recarga fue de: ',
+                              style: DefaultTextStyle.of(context).style,
+                              children: <TextSpan>[
+                                TextSpan(
+                                    text:
+                                        '\$${virtualCtrl.virtualWalletPassenger.value?.lastRechargeValue}',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                              ],
+                            ),
+                          );
+                        })
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            )
-          : Center(child: Text('Billetera no disponible')),
-    );
+              )
+            : Center(child: Text('Billetera no disponible')),
+      );
+    });
   }
 }
